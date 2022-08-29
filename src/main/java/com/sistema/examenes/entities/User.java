3372aa76faf166991d93716d69b40f65f3cfbe9b
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-08-29 14:02:04 -0500

    fix: add jsonignore to avoid infinite cycle

--- src/main/java/com/sistema/examenes/entities/User.java
+++ src/main/java/com/sistema/examenes/entities/User.java
@@ -1,5 +1,7 @@
 package com.sistema.examenes.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,6 +24,7 @@ public class User {
     private String profile;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    @JsonIgnore
     private Set<UserRol> userRoles = new HashSet<>();
 
     public User() {
