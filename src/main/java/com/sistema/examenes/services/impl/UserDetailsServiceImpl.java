529668ed34bad3a2b71a8b18dce03bcaa61291b8
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-08-30 09:26:40 -0500

    feat: add method to load user by username

--- src/main/java/com/sistema/examenes/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/sistema/examenes/services/impl/UserDetailsServiceImpl.java
@@ -1,4 +1,25 @@
 package com.sistema.examenes.services.impl;
 
-public class UserDetailsServiceImpl {
+import com.sistema.examenes.entities.User;
+import com.sistema.examenes.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User Not found");
+        }
+        return user;
+    }
 }
