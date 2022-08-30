0d66b83ea7008c420a0c3ee1bf1070e97a41eaf0
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-08-30 09:18:23 -0500

    Add Class Authority that implement GrantedAuthority

--- src/main/java/com/sistema/examenes/entities/Authority.java
+++ src/main/java/com/sistema/examenes/entities/Authority.java
@@ -1,4 +1,17 @@
 package com.sistema.examenes.entities;
 
-public class Authority {
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
 }
