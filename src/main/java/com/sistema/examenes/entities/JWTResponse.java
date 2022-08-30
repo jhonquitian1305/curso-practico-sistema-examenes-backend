ecaa8350d69b781b03561120c1513f28d83cb9a6
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-08-30 09:19:33 -0500

    Add class JWTResponse

--- src/main/java/com/sistema/examenes/entities/JWTResponse.java
+++ src/main/java/com/sistema/examenes/entities/JWTResponse.java
@@ -1,4 +1,21 @@
 package com.sistema.examenes.entities;
 
 public class JWTResponse {
+
+    private String token;
+
+    public JWTResponse() {
+    }
+
+    public JWTResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
